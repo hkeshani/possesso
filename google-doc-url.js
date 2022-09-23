723c7e48b214ebde1114618add9935e3c214d324
hkeshani <ubco.arth.keshani@gmail.com>
2022-09-23 13:10:05 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQQrfZ_B8lnsqaG37Ov1XVJNZO-tAsvsn-08tcqBI3dXSQE9rw_bXatNLuORaIgeV2hmyBG2TEW7wdK/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o330LLn4cXYFJRwspTVjkcedOZo8xGnnu5oOruhNUv8/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
